40546a7e650e48ccd1389f23f7d92ea461131073
Ivette C Valdivia Gomez <89611005+ivette2021@users.noreply.github.com>
2023-11-06 16:42:50 -0300

    comparativa entre usar click animacion y click en base a estado de animacion

--- app/src/main/java/com/example/jeckpackcomposecatalog/ui/theme/AnimationExample.kt
+++ app/src/main/java/com/example/jeckpackcomposecatalog/ui/theme/AnimationExample.kt
@@ -1,8 +1,11 @@
 package com.example.jeckpackcomposecatalog.ui.theme
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,13 +18,26 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun ColorAnimationSimple(){
-    var firstColor by rememberSaveable{
-        mutableStateOf(false)
+    Column() {
+        var firstColor by rememberSaveable{
+            mutableStateOf(false)
+        }
+        var realColor = if (firstColor) Color.Red else Color.Yellow
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(realColor)
+            .clickable { firstColor = !firstColor }) 
+        Spacer(modifier = Modifier.size(200.dp))
+
+        var secondColor by rememberSaveable{
+            mutableStateOf(false)
+        }
+        val realColor2 by animateColorAsState(targetValue = if (secondColor) Color.Red else Color.Yellow )
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(realColor2)
+            .clickable { secondColor = !secondColor })
+        Spacer(modifier = Modifier.size(200.dp))
     }
-    var realColor = if (firstColor) Color.Red else Color.Yellow
-    Box(modifier = Modifier
-        .size(100.dp)
-        .background(realColor)
-        .clickable { firstColor =!firstColor }) {
-}
+
 }
