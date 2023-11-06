ac237685873485f2900dcb4d2fad30a2f74079fb
Ivette C Valdivia Gomez <89611005+ivette2021@users.noreply.github.com>
2023-11-06 18:14:44 -0300

    animacion de visibilidad avanzada, viendo algunos tipos de animaciones de la libreria

--- app/src/main/java/com/example/jeckpackcomposecatalog/ui/theme/AnimationExample.kt
+++ app/src/main/java/com/example/jeckpackcomposecatalog/ui/theme/AnimationExample.kt
@@ -1,9 +1,21 @@
 package com.example.jeckpackcomposecatalog.ui.theme
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.animation.shrinkOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -24,16 +36,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun ColorAnimationSimple(){
-        var firstColor by rememberSaveable{
-            mutableStateOf(false)
-        }
+fun ColorAnimationSimple() {
+    var firstColor by rememberSaveable {
+        mutableStateOf(false)
+    }
     var showBox by rememberSaveable {
         mutableStateOf(true)
     }
     val realColor by animateColorAsState(targetValue = if (firstColor) Color.Red else Color.Yellow,
         animationSpec = tween(durationMillis = 2000),//2 segubdos para hacer una transicion mas lenta
-        finishedListener = {showBox =false} //desaparesca
+        finishedListener = { showBox = false } //desaparesca
     )
     if (showBox) {
         Box(modifier = Modifier
@@ -42,30 +54,65 @@ fun ColorAnimationSimple(){
             .clickable { firstColor = !firstColor })
     }
 }
+
 @Composable
-fun SizeAnimation(){
+fun SizeAnimation() {
     var smallSize by rememberSaveable {
         mutableStateOf(true)
     }
-    val size by animateDpAsState(targetValue = if(smallSize) 50.dp else 100.dp,animationSpec = tween(durationMillis = 500))
+    val size by animateDpAsState(
+        targetValue = if (smallSize) 50.dp else 100.dp,
+        animationSpec = tween(durationMillis = 500)
+    )
     Box(modifier = Modifier
         .size(size)
         .background(Color.Cyan)
         .clickable { smallSize = !smallSize })
 }
+
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun VisibilityAnimation(){
-    var isVisible by remember{ mutableStateOf(true)}
+fun VisibilityAnimation() {
+    var isVisible by remember { mutableStateOf(true) }
 
     Column(Modifier.fillMaxSize()) {
         Button(onClick = { isVisible = !isVisible }) {
             Text("Mostrar/ocultar")
         }
-        Spacer(modifier = Modifier.size(50.dp))
+        Spacer(modifier = Modifier.size(30.dp))
 
-       AnimatedVisibility (isVisible){
-        Box(Modifier.size(150.dp).background(Color.Red))
+        AnimatedVisibility(isVisible, enter = slideInVertically(), exit = slideOutVertically()) {
+            Box(
+                Modifier
+                    .size(100.dp)
+                    .background(Color.Red))
 
         }
+
+        Spacer(modifier = Modifier.size(30.dp))
+        AnimatedVisibility(isVisible, enter = slideInHorizontally(), exit = slideOutHorizontally()) {
+            Box(
+                Modifier
+                    .size(100.dp)
+                    .background(Color.Red))
+
+        }
+        Spacer(modifier = Modifier.size(30.dp))
+        AnimatedVisibility(isVisible, enter = scaleIn(), exit = scaleOut()) {
+            Box(
+                Modifier
+                    .size(100.dp)
+                    .background(Color.Red))
+
+        }
+        Spacer(modifier = Modifier.size(30.dp))
+        AnimatedVisibility(isVisible, enter = fadeIn(), exit = fadeOut()) {
+            Box(
+                Modifier
+                    .size(100.dp)
+                    .background(Color.Red))
+
+        }
+
     }
 }
