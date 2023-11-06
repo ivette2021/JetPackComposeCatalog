5e458f02442db0e1bb00da2db308b142c0e4d39f
Ivette C Valdivia Gomez <89611005+ivette2021@users.noreply.github.com>
2023-11-06 17:33:21 -0300

    animacion de desvanecimiento despues de unos sg

--- app/src/main/java/com/example/jeckpackcomposecatalog/ui/theme/AnimationExample.kt
+++ app/src/main/java/com/example/jeckpackcomposecatalog/ui/theme/AnimationExample.kt
@@ -1,6 +1,7 @@
 package com.example.jeckpackcomposecatalog.ui.theme
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -18,26 +19,34 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun ColorAnimationSimple(){
-    Column() {
         var firstColor by rememberSaveable{
             mutableStateOf(false)
         }
-        var realColor = if (firstColor) Color.Red else Color.Yellow
+    var showBox by rememberSaveable {
+        mutableStateOf(true)
+    }
+    val realColor by animateColorAsState(targetValue = if (firstColor) Color.Red else Color.Yellow,
+        animationSpec = tween(durationMillis = 2000),//2 segubdos para hacer una transicion mas lenta
+        finishedListener = {showBox =false} //desaparesca
+    )
+    if (showBox) {
         Box(modifier = Modifier
             .size(100.dp)
             .background(realColor)
-            .clickable { firstColor = !firstColor }) 
-        Spacer(modifier = Modifier.size(200.dp))
-
-        var secondColor by rememberSaveable{
-            mutableStateOf(false)
-        }
-        val realColor2 by animateColorAsState(targetValue = if (secondColor) Color.Red else Color.Yellow )
-        Box(modifier = Modifier
-            .size(100.dp)
-            .background(realColor2)
-            .clickable { secondColor = !secondColor })
-        Spacer(modifier = Modifier.size(200.dp))
+            .clickable { firstColor = !firstColor })
     }
+
+//        Spacer(modifier = Modifier.size(200.dp))
+//
+//        var secondColor by rememberSaveable{
+//            mutableStateOf(false)
+//        }
+//        val realColor2 by animateColorAsState(targetValue = if (secondColor) Color.Red else Color.Yellow )
+//        Box(modifier = Modifier
+//            .size(100.dp)
+//            .background(realColor2)
+//            .clickable { secondColor = !secondColor })
+
+
 
 }
